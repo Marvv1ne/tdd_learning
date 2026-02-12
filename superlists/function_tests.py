ee2064af4f66256abce65c6472d7dc6fffbd6d0a
Marvv1ne <gorn89@inbox.ru>
2026-02-12 21:58:18 +0400

    feat: add functional tests

--- superlists/function_tests.py
+++ superlists/function_tests.py
@@ -1,6 +1,27 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "Django" in browser.title
+class NewVisitorTest(unittest.TestCase):
+    "Тест первого посетителя"
+
+    def setUp(self) -> None:
+        "Установка"
+
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        "Демонтаж"
+
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrive_it_later(self) -> None:
+        self.browser.get("http://localhost:8000")
+
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Need to finish test")
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
