81d040894c845c83fa1a99e90d62b08525a224d2
Marvv1ne <gorn89@inbox.ru>
2026-02-19 17:08:41 +0400

    feat: add functions test with  add new items on home page

--- superlists/function_tests.py
+++ superlists/function_tests.py
@@ -19,14 +19,20 @@ class NewVisitorTest(unittest.TestCase):
 
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """Подтверждение строки в таблице списка"""
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrive_it_later(self) -> None:
         # Посетитель открывает браузер Firefox заходит на домашнюю страницу веб приложения
         self.browser.get("http://localhost:8000")
 
         # На домашней странице посетитель видит заголовок To-Do
         self.assertIn("To-Do", self.browser.title)
-        header_text = self.browser.find_elements(By.TAG_NAME, "h1")
-        self.assertIn("To-Do", header_text)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1")
+        self.assertEqual("Your To-Do list", header_text.text)
 
         # Посетителю сразу же предлагается ввести элемент списка
         inputbox = self.browser.find_element(By.ID, "id_new_item")
@@ -41,17 +47,20 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertTrue(any(row.text == "1: Изучить TDD" for row in rows))
 
         # Текстовое поле по-прежнему предлагает добавить еще один элемент
         # Пользователь вводить "изучить DDD"
 
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Изучить DDD")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertTrue(any(row.text == "2: Изучить DDD" for row in rows))
+
+        self.check_for_row_in_list_table("1: изучит TDD")
+
+        self.check_for_row_in_list_table("2: изучит DDD")
 
         self.fail("Need to finish test")
 
