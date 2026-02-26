97e0eccfae7e08e320a298c447d6bf23c31ef7bb
Marvv1ne <gorn89@inbox.ru>
2026-02-26 11:26:33 +0400

    feat: add tests to view_list view

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils import text
 from lists.models import Item
 
 
@@ -24,23 +25,13 @@ class HomePageTest(TestCase):
         """тест: переадресует после post-запроса"""
         response = self.client.post("/", data={"item_text": "A new list item"})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response["location"], "/")
+        self.assertEqual(response["location"], "/lists/one-in-the-world-list/")
 
     def test_only_saves_items_when_necessary(self):
         """тест: сохраняет элементы, только когда нужно"""
 
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_display_all_list_items(self):
-        """тест: отображаются все элементы списка"""
-
-        Item.objects.create(text="item 1")
-        Item.objects.create(text="item 2")
-
-        response = self.client.get("/")
-        self.assertContains(response, "item 1")
-        self.assertContains(response, "item 2")
 
 
 class ItemModelTest(TestCase):
@@ -64,3 +55,24 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
         self.assertEqual(second_saved_item.text, "Item the second")
+
+
+class ListViewTest(TestCase):
+    """Тест представления списка"""
+
+    def test_uses_list_template(self):
+        """тест: используется шаблон списка"""
+
+        response = self.client.get("/lists/one-in-the-world-list/")
+        self.assertTemplateUsed(response, "list.html")
+
+    def test_display_all_items(self):
+        """тест: отображаются все элементы списка"""
+
+        Item.objects.create(text="item 1")
+        Item.objects.create(text="item 2")
+
+        response = self.client.get("/lists/one-in-the-world-list/")
+
+        self.assertContains(response, "item 1")
+        self.assertContains(response, "item 2")
