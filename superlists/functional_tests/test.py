92bebe1b5b476cf920b0454a2adc1c989ed7adcc
Marvv1ne <gorn89@inbox.ru>
2026-03-03 12:23:00 +0400

    feat: make test for maket and style of pages

--- superlists/functional_tests/test.py
+++ superlists/functional_tests/test.py
@@ -116,3 +116,24 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element(By.TAG_NAME, "body").text
         self.assertNotIn("Изучить TDD", page_text)
         self.assertIn("Изучить ООП", page_text)
+
+    def test_layout_and_styling(self):
+        """тест макета и стилевого оформления"""
+        # Пользователь открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Он замечает, что поле ввода аккуратно центрировано
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10
+        )
+        # Он начинает новый список и видит, что полу ввода там тоже
+        # аккуратно центрировано
+        inputbox.send_keys("testing")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: testing")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10
+        )
