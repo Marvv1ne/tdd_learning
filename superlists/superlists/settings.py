0b70f34a911eca08731b56a0d0c8b61c2494c64b
Marvv1ne <gorn89@inbox.ru>
2026-03-05 13:35:13 +0400

    feat: move database path to root dir of project

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -14,8 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -77,7 +76,7 @@ WSGI_APPLICATION = "superlists.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "database" / "db.sqlite3",
     }
 }
 
