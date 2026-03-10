b3b3a4447f13f5c56f085a08e8d1bae21d7ab46e
Marvv1ne <gorn89@inbox.ru>
2026-03-10 15:30:32 +0400

    fix: remove not used imports

--- superlists/functional_tests/base.py
+++ superlists/functional_tests/base.py
@@ -1,10 +1,8 @@
 import time
 import os
-from unittest import skip
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 
