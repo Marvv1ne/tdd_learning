eb472ec4e4b6caaf8179d8d1513503d148c0130e
Marvv1ne <gorn89@inbox.ru>
2026-03-11 13:03:35 +0400

    feat: add test on validation error

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils.html import escape
 from lists.models import Item, List
 
 
@@ -10,38 +11,6 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
 
         self.assertTemplateUsed(response, "home.html")
-
-
-class LIstAndItemModelTest(TestCase):
-    """тест модели элемента списка"""
-
-    def test_saving_and_retrieviing_items(self):
-        """тест сохранения и получения элементов списка"""
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) list item")
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, "Item the second")
-        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -101,6 +70,16 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f"/lists/{new_list.id}/")
 
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        """тест: ошибки валидации отсылаются назад
+        в шаблон домашней страницы"""
+
+        response = self.client.post("/lists/new", data={"item_text": ""})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
 
 class NewItemTest(TestCase):
     """тест нового элемента списка"""
@@ -134,4 +113,3 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f"/lists/{correct_list.id}/")
-
